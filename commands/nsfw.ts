7c270ac37fdba82387068cec63ce149d61484d84
Ilya Soloveychik <dj.active.17@gmail.com>
2022-02-23 21:54:48 +0200

    feat: remove express (will be separated), add nsfw query handle

--- commands/nsfw.ts
+++ commands/nsfw.ts
@@ -1,9 +1,65 @@
+import axios from 'axios';
+import { config } from 'dotenv';
 import { ICommand } from 'wokcommands';
+import getRandomInt from '../utils/getRandomInt';
+import logger from '../utils/logger';
+
+if (process.env.NODE_ENV!.startsWith('dev')) config();
+
+const nsfwHandler = async (args: string[]) => {
+
+    try {
+        // define if we should search by word
+        const query = args.length !== 0 && args[0] ? `/${args[0]}` : null; 
+
+        const response = await axios.get(
+            `${process.env.SERVER_URL}/nsfws/search${query ? query : ''}`,
+            {
+                headers: { 'x-api-key': process.env.API_KEY!.toString() }
+            }
+        );
+        const data = await response.data.data;
+        let msg;
+
+        if (query) {
+            // If empty array (nothing was found)
+            if (data.length === 0) return 'Бот ничего не нашел!';
+            // Otherwise, create one big message from results
+            const msgParts = data.map((item: any) => `
+                ${item.he}
+                ${item.translit}
+                ${item.ru}
+            `);
+
+            return `
+                https://sababushka.com/sex
+                ${msgParts.join('\n')}
+            `;
+
+        } else {
+            // random word in case no query provided
+            const index = getRandomInt(0, data.length - 1);
+
+            msg = `
+                https://sababushka.com/sex
+                ${data[index].he}
+                ${data[index].translit}
+                ${data[index].ru}
+            `
+
+        }
+
+        return msg;
+    } catch (error) {
+        logger.error(error);
+        return `Произошла ошибка: ${error}`;
+    }
+}
 
 export default {
     category: 'Misc',
-    description: 'Слова на тему 18+ ;)',
+    description: 'Слова на тему 18+ ;) Если не ввести параметры поиска, будет показано случайное слово',
     slash: 'both',
     // testOnly: true,
-    callback: () => 'nsfw'
+    callback: ({ args }) => nsfwHandler(args)
 } as ICommand
--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
 import path from 'path';
 import dotenv from 'dotenv';
-import express from 'express';
 import WOKCommands from 'wokcommands';
 import { Client } from 'discord.js';
 import { cronsInit } from './crons';
 import logger from './utils/logger';
-import adminCheck from './utils/auth';
-import addToSababushka from './addToSababushka';
 if (process.env.NODE_ENV === 'dev'){
     logger.warn('dev mode');
     dotenv.config();
@@ -20,30 +17,19 @@ const client = new Client({
     ]
 });
 
-
-// HTTP route for adding info
-const app = express();
-app.use(express.json());
-
-app.post('/addToSababushka', adminCheck, addToSababushka);
-
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
-app.listen(SERVER_PORT, () => logger.info(`HTTP server started @ ${SERVER_PORT}`));
-
-
 // Discord bot init
-client.on('ready', () => {
+client.on('ready', async () => {
     logger.info(`logged: ${client.user!.tag}`);
+    await cronsInit();
     
     new WOKCommands(client, {
         commandsDir: path.join(__dirname, 'commands'),
         typeScript: true,
         testServers: ['938850851810865264'],
         mongoUri: process.env.MONGO,
-    })
+    });
     
 
-    cronsInit(client);
 });
 
 
