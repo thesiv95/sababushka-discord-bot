2db64a727b357929202b847bc2a9e33d081c8050
Ilya Soloveychik <dj.active.17@gmail.com>
2022-04-15 13:55:26 +0300

    fix: cmds init in production mode

--- index.ts
+++ index.ts
@@ -20,12 +20,14 @@ const client = new Client({
 client.on('ready', async () => {
     logger.info(`logged: ${client.user!.tag}`);
     await cronsInit();
+    const isDevMode = process.env.NODE_ENV === 'dev';
+    // const cmdsPath = isDevMode ? 'commands' : 'dist/commands';
     
     new WOKCommands(client, {
         // 1 file = 1 command
         commandsDir: path.join(__dirname, 'commands'),
         // we use ts only in dev mode. otherwise cmds will not be registered
-        typeScript: process.env.NODE_ENV === 'dev',
+        typeScript: isDevMode,
         // ids are a single line separated with commas in env, here we need to convert it to array
         testServers: process.env.TEST_DISCORD_SERVERS_IDS!.split(','),
         // mongodb is used for wokcommands params, and for words as well
